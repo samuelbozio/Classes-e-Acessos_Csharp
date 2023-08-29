67aa5b6da2e611797d193c8b2bd58691e25a9148
Samuel Bozio <samuelbozio@hotmail.com>
2023-08-29 19:52:01 -0300

    classes e metodos de acessos no c#

---
+++ Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using Classes;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Produto produto = new Produto("TV", 40.00, 10);
+
+            
+
+            produto.Nome = "Banana";
+
+            Console.WriteLine(produto.Nome);
+        }
+    }
+}
---
+++ Classes/Triangulo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Produto
+    {
+
+        private string _nome;
+
+        public int MyProperty { get; set; }
+        public double Preco { get; private set; }
+        private int _quantidade;
+
+        public Produto(string nome, double valor, int quantidade)
+        {
+            _nome = nome;
+            Preco = valor;
+            _quantidade = quantidade;
+        }
+
+       public string Nome
+        {
+            get => _nome;
+                
+            set {
+                _nome = value;
+            }
+        }
+
+
+    }
+}
